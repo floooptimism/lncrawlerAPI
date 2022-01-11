3e0117fc6103b2bc9a9ee5b73abb290d2803511c
Goblin <jackmord445@gmail.com>
2022-01-11 22:41:59 +0800

    Fixed Scraper Manager

--- components/ScraperManager/ScraperManager.js
+++ components/ScraperManager/ScraperManager.js
@@ -1,17 +1,22 @@
+const parse = require('url-parse');
+
 class ScraperManager{
     constructor(){
         this.scrapers = {};
     }
 
     getScraperBySource(scraperSource){
-        if(!this.scrapers[scraperSource]){
-            throw new Error(`Scraper with source ${scraperSource} not found`);
+        let host = parse(scraperSource).host;
+        if(!this.scrapers[host]){
+            return null
+            // throw new Error(`Scraper with source ${host} not found`);
         }
-        return this.scrapers[scraperSource];
+        return this.scrapers[host];
     }
 
     registerScraper(scraper){
-        let scraperSource = scraper.getSource();
+        let scraperSource = parse(scraper.getSource()).host;
+        console.log("Registering scraper with source: " + scraperSource);
         if(this.scrapers[scraperSource]){
             throw new Error(`Scraper with source ${scraperSource} already exists`);
         }
--- components/ScraperManager/init.js
+++ components/ScraperManager/init.js
@@ -1,7 +1,22 @@
+const Scraper = require("../Scraper/Scraper");
 const ScraperManager = require("./ScraperManager");
+
+
+// SCRAPERS
+
+    // www.lightnovel.world
+    const LightNovelDotWorldIE = require("../../subcomponents/InformationExtractor/concretes/LightNovelDotWorldIE");
+    const DefaultPageFetcher = require("../../subcomponents/PageFetcher/concretes/DefaultPageFetcher");
+    const LightNovelDotWorldScraper = require("../Scraper/concrete/LightNovelDotWorldScraper");
+
+
+
+
+    
 
 let scraperManager = new ScraperManager();
 
 //register your scrapers here
+scraperManager.registerScraper(new LightNovelDotWorldScraper(LightNovelDotWorldIE, DefaultPageFetcher));
 
 module.exports = scraperManager;
